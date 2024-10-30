ebfc1147f4779e4d8b6b34a14cf4e59319fcfa06
Bernardo Carvalho <ber.cai.car@gmail.com>
2024-10-30 23:47:33 +0000

    Created branch

--- app/src/main/java/pt/iade/ei/zoopolis/ui/components/StartingMenu.kt
+++ app/src/main/java/pt/iade/ei/zoopolis/ui/components/StartingMenu.kt
@@ -1,42 +1,73 @@
 package pt.iade.ei.zoopolis.ui.components
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import pt.iade.ei.zoopolis.R
+
 
 @Composable
 
-fun StartingMenu(){
+fun StartingMenu() {
 
-    Column(
-        modifier = Modifier
-            .background(color = Color(0xFF006600)) // Background color
-
-) {
-        Text(
-            text = "Login",
-            fontWeight = FontWeight.Bold,
-            fontSize = 9.sp
+    Box {
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            painter = painterResource(R.drawable.startingmenubackground),
+            contentDescription = "background_image",
+            contentScale = ContentScale.FillBounds
         )
+        Scaffold(
+            containerColor = Color.Transparent
+        ) { innerPadding ->
+            Box(
 
-        Text(
-            text = "Register",
-            fontWeight = FontWeight.Bold,
-            fontSize = 8.sp,
-            modifier = Modifier.padding(top = 30.dp)
-        )
+                contentAlignment = Alignment.TopCenter,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 0.dp)
 
+            ) {
+                Column(
+                    modifier = Modifier
+                        .padding(innerPadding)
+                        .fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.logozoo),
+                        contentDescription = "Logo",
+                        contentScale = ContentScale.Fit,
+                        modifier = Modifier.size(300.dp, 300.dp),
+                        alignment = Alignment.TopStart
+                    )
+
+
+
+                    Login("Login", R.drawable.login)
+                    Login("Register", R.drawable.register)
+                    Login("Enter as Guest", R.drawable.guest)
+
+                }
+            }
+        }
     }
 }
-
 
 @Preview(showBackground = true)
 @Composable
